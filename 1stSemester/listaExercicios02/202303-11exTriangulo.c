dd79fcf5e3f7ed4a0be6134bd676770b2feb0dcf
Renato Pires <renato696@gmail.com>
2023-05-02 06:51:52 -0300

    add triangle test
    added a test to check if the triangle exists.

--- 1stSemester/listaExercicios02/202303-11exTriangulo.c
+++ 1stSemester/listaExercicios02/202303-11exTriangulo.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-	int ladoA, ladoB, ladoC;
+	int ladoA, ladoB, ladoC, ladoMaior, lado1, lado2;
 
 	printf("Insira o tamanho do lado A do triangulo: ");
 	scanf("%d", &ladoA);
@@ -12,16 +12,32 @@ int main(){
 	printf("\nInsira o tamanho do lado C do triangulo: ");
 	scanf("%d", &ladoC);
 
-	if((ladoA == ladoB) && (ladoB == ladoC)){
-		printf("\nTrata-se de triangulo equilatero\n");}
-	else if((ladoA == ladoB) && (ladoB != ladoC)){
-		printf("\nTrata-se de triangulo isosceles\n");}
-	else if((ladoA == ladoC) && (ladoC != ladoB)){
-		printf("\nTrata-se de triangulo isosceles\n");}	
-	else if((ladoB == ladoC) && (ladoC != ladoA)){
-		printf("\nTrata-se de triangulo isosceles\n");}
-	else {
-		printf("\nTrata-se de triangulo escaleno\n");}
+	if(ladoB > ladoA){
+		ladoMaior = ladoB;
+		lado1 = ladoA;
+		lado2 = ladoC;}
+	else if((ladoC > ladoA) || (ladoC > ladoB)){
+		ladoMaior = ladoC;
+		lado1 = ladoA;
+		lado2 = ladoB;}
+	else { 
+		ladoMaior = ladoA;
+		lado1 = ladoB;
+		lado2 = ladoC;
+	}
+
+
+	if((lado1+lado2) > ladoMaior){
+		if((ladoA == ladoB) && (ladoB == ladoC)){
+			printf("\nTrata-se de triangulo equilatero\n");}
+		else if((ladoA == ladoB) && (ladoB != ladoC)){
+			printf("\nTrata-se de triangulo isosceles\n");}
+		else if((ladoA == ladoC) && (ladoC != ladoB)){
+			printf("\nTrata-se de triangulo isosceles\n");}	
+		else if((ladoB == ladoC) && (ladoC != ladoA)){
+			printf("\nTrata-se de triangulo isosceles\n");}
+		else { printf("\nTrata-se de triangulo escaleno\n");}
+	} else { printf("\nAs medidas informadas não formam um triangulo\n");}
 	
 	return 0;
 }
