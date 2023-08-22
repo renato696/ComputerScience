d934c0fa2be92bc0c1e38c1e61752afae51a3630
Renato Pires <68570832+renato696@users.noreply.github.com>
2023-08-22 20:26:52 -0300

    exercicios em aula

---
+++ 2ndSemester/EstruturaDeDados/20230823ExercicioSwitch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(){
+    char letra;
+
+    printf("digite uma letra para saber se trata-se de vogal ou consoante:\n");
+    scanf("%c", &letra);
+
+    switch(letra){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u': printf("trata-se de uma vogal");
+        break;
+        default: printf("trata-se de uma consoante");
+        break;
+    };
+
+}
