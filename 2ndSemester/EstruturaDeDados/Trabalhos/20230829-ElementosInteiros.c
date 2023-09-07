96de87bdb5e24afc27158c5899978becf7daf57e
Renato Pires <68570832+renato696@users.noreply.github.com>
2023-09-07 14:12:56 -0300

    inclusao dos exemplos do exercicio

--- 2ndSemester/EstruturaDeDados/Trabalhos/20230829-ElementosInteiros.c
+++ 2ndSemester/EstruturaDeDados/Trabalhos/20230829-ElementosInteiros.c
@@ -1,3 +1,10 @@
 /*crie um programa que leia 10 elementos inteiros e mostre a quantidade
-e numeros primos dentre os numeros que foram digitados:*/
+e numeros primos dentre os numeros que foram digitados:
+Exemplo: 
+4
+7
+12
+5... => existem 3 numeros primos*/
+
+
 
