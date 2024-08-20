1682a50a0551745fa4657f5d185c1af9d2663873
renato696 <renato696@gmail.com>
2024-08-20 19:44:14 -0300

    media de notas

--- 4thSemester/Python/20240820 - exercicios.py
+++ 4thSemester/Python/20240820 - exercicios.py
@@ -23,3 +23,26 @@ else:
     print(f'Seu salário com bonus é R$: {salario_com_bonus}')
 """
 
+"""
+Calcular média das notas e informar se aprovado/recuperacao/reprovado
+START
+- insira a nota de cada prova e armazene nas variáveis
+- validar cada nota se entre 0-10
+- calcule a média das notas
+- comparar a media com as notas alvo e informar se aprovado/recuperacao/reprovado
+"""
+
+print('Média das notas e resultado final')
+
+nota1 = int(input('Informe a nota da sua primeira nota: '))
+nota2 = int(input('Informe a nota da sua segunda nota: '))
+nota3 = int(input('Informe a nota da sua terceira nota: '))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7:
+    print(f'Sua média é {media}, você foi aprovado!')
+elif  5 <= media < 7:
+    print(f'Sua média é {media}, você ficou de recuperação')
+else:
+    print(f'Sua média foi {media}, você reprovou.')
