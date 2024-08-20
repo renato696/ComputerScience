fda7bbdb1c90cdc1fc99a669f609be97359ab609
renato696 <renato696@gmail.com>
2024-08-20 19:27:24 -0300

    ex-Calculadora_bonus_salario

--- 4thSemester/Python/20240820 - exercicios.py
+++ 4thSemester/Python/20240820 - exercicios.py
@@ -1,2 +1,25 @@
 #Lista de exercícios
 
+"""
+ex13 - Calculo de bonus
+START
+- perguntar o salario
+- numero de anos de serviço na empresa
+- mais de 5 anos -> bonus 10%
+- menos de 5 anos -> bonus 5%
+END
+
+
+print('Calculadora de Bonus Salarial')
+
+salario = float(input('Qual o valor do seu salário? '))
+anos_de_trabalho = int(input('Quantos anos de trabalho na empresa você tem? '))
+
+if anos_de_trabalho >= 5:
+    salario_com_bonus = salario * 1.1
+    print(f'Seu salário com bonus é R$: {salario_com_bonus}')
+else: 
+    salario_com_bonus = salario * 1.05
+    print(f'Seu salário com bonus é R$: {salario_com_bonus}')
+"""
+
