333e5ff5e0711808b751cd7cddddea9922844627
renato696 <renato696@gmail.com>
2024-08-27 20:55:26 -0300

    range e soma

--- 4thSemester/Python/20240827 - aula.py
+++ 4thSemester/Python/20240827 - aula.py
@@ -8,7 +8,6 @@ for item in itens:
 nome = input("Entre com seu nome: \n")
 for letra in nome:
     print(letra)
-"""
 
 colegas = []
 
@@ -17,3 +16,17 @@ for i in range(4):
     colegas.append(nome_dos_colegas)
 
 print(colegas)
+
+for numero in range(1, 11):
+    if numero % 2 == 0: print(f"{numero} é par")
+    else: print(f"{numero} é impar")
+"""
+
+numero = int(input('Digite um número: '))
+soma = 0
+
+for i in range(3, numero, 1*3):
+    print(i)
+    soma = soma + i
+    
+print(f"O resultado da soma é: {soma}")
