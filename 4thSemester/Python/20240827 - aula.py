873f3e97681bce27ce1f180b3b23e5a831f58ba5
renato696 <renato696@gmail.com>
2024-08-27 20:02:50 -0300

    append

--- 4thSemester/Python/20240827 - aula.py
+++ 4thSemester/Python/20240827 - aula.py
@@ -3,8 +3,17 @@ itens = [10, 20, 30, 40, 50]
 
 for item in itens:
     print(item)
-"""
+
 
 nome = input("Entre com seu nome: \n")
 for letra in nome:
     print(letra)
+"""
+
+colegas = [] # lista vazia, tbm posso fazer com dicionários, tuplas
+
+for i in range(4):
+    nome_dos_colegas = input('Digite o nome do colega: ')
+    colegas.append(nome_dos_colegas)
+
+print(colegas)
